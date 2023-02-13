dfe180a798f8f6276ecfa109408590918fe396f7
borishaus <boris.a.clark@icloud.comgit config --global user.email boris.a.clark@icloud.comgit config --global user.email boris.a.clark@icloud.com>
2023-02-13 18:48:14 -0500

    feb13commit 1

--- week_1/week_1.R
+++ week_1/week_1.R
@@ -1,1 +1,12 @@
-#new script for week 1 homework, hopefully not submitted too late. 
+#new script for week 1 homework, hopefully not submitted too late.   
+#part 4A
+y <- c()
+x <- c()
+for(i in 1:100){
+  x<-append(x,i^3)
+}
+for(j in 1:100){
+  y <- append(y,x[j]-mean(x))
+}
+print(mean(y)) # outputs 0
+print(sd(x)-sd(y)) # outputs 0
