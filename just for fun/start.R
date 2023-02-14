16c161c495e8efc7b56e45c89038576ae0c991d4
borishaus <boris.a.clark@icloud.comgit config --global user.email boris.a.clark@icloud.comgit config --global user.email boris.a.clark@icloud.com>
2023-02-13 22:37:02 -0500

    just for fun

--- just for fun/start.R
+++ just for fun/start.R
@@ -1,9 +1,29 @@
-varY<-c(1:100)
-varX<-c(200:300)
-#this is the place to run R 
+pall<-c()
+x<-c()
+y<-c()
+ct<-30000/4
 
-varW <- c(700:10000)
-print(1:100)
-print(1:100)
-print(1:100)
-print(1:100)
+for (i in 1:ct) {
+  pall<-append(pall,'red')
+}
+
+for (i in 1:ct) {
+  pall<-append(pall,'blue')
+}
+
+for (i in 1:ct) {
+  pall<-append(pall,'yellow')
+}
+
+for (i in 1:ct) {
+  pall<-append(pall,'yellow')
+}
+
+x<- runif(30000,0,1)
+
+for(i in 1:30000){
+  y<-append(y,i)
+}
+plot(x,y, col=pall, main="COLOMBIA")
+
+
